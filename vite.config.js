9076677187923f8e0d686f72e289d9b285892e9d
hunte <besscomdark@gmail.com>
2025-09-26 07:06:32 +0800

    change commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-react-app/', // 你的 repo 名稱，最後要有斜線
+  base: '/my-react-todo/', // 你的 repo 名稱，最後要有斜線
   plugins: [react()]
 })
