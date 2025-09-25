4156edc96c2837ef436ea4674446c5b423658f70
hunte <besscomdark@gmail.com>
2025-09-25 22:48:50 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/my-react-app/', // 你的 repo 名稱，最後要有斜線
+  plugins: [react()]
 })
